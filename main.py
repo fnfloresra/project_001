9dc43d9a379dde19580a6c8043d434fe3573882c
fernando flores <fernando.floresr@unmsm.edu.pe>
2024-02-07 22:41:42 -0500

    Update main.py
    next commit

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ print("The square root of 2 is", a)
 
 print("hi there!")
 
+b = math.cos(math.pi / 4)   # The cosine of pi/4 (which is the
+
+c = math.sin(math.pi / 4)    # angle whose tangent is 1, and which is
+
+d = b/c
+
+print(d)
+
